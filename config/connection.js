2cb3afa2527bed57f09b58b13106107219a421ea
benjamin amakye <bamakye20@gmail.com>
2022-04-08 06:20:28 -0400

    make it better

--- config/connection.js
+++ config/connection.js
@@ -4,13 +4,13 @@ require('dotenv').config();
 let sequelize;
 // create connection to our db
 if (process.env.JAWSDB_URL) {
-    //sequelize = new Sequelize(process.env.JAWSDB_URL);
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
     sequelize = new Sequelize('blog_post', 'root', '@Ambitious03!');
 } else {
-    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    sequelize = new Sequelize('blog_post', 'root', '@Ambitious03!', {
         host: 'localhost',
         dialect: 'mysql',
-        port: 3306
+        port: 3306 
     });
 }
 
